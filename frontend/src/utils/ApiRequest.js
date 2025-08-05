bc1829d673245b4a7a5465cbf81bc9656e4a353e
Bhavyapratap0001 <bhavyapratap0707@gmail.com>
2025-08-05 14:06:57 +0530

    Prepare frontend for production deployment

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,9 +1,9 @@
 
 
-import axios from "axios";
+// import axios from "axios";
 
-// const host = "https://expense-tracker-app-knl1.onrender.com";
-const host = "http://localhost:5005";
+const host = "https://my-expense-tracker-0mph.onrender.com";
+// const host = "http://localhost:5005";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
@@ -14,13 +14,13 @@ export const deleteTransactions = `${host}/api/v1/deleteTransaction`;
 
 
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5005';
+// const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5005';
 
-const api = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// const api = axios.create({
+//   baseURL: `${API_BASE_URL}/api`,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
-export default api;
+// export default api;
